9452720124ace43ea3c67e95136d36c893d8d0d9
fenfei <guofenfei1991@gmail.com>
2015-11-01 17:06:56 -0700

    add save model

--- main.py
+++ main.py
@@ -14,3 +14,4 @@ if __name__ == '__main__':
     model = SkipGramNegSampEmbeddingModel(dimension=64)
     model.build_vocab(text_generator("./data/tiny_text8"))
     model.fit(text_generator("./data/tiny_text8"), sampling=False)
+    model.save_model("/mnt/my_data/model/my_models")
--- models.py
+++ models.py
@@ -1,11 +1,12 @@
 # -*- coding:utf-8 -*-
 
 import theano
+import os
 
 from numpy import random, zeros
 from theano import tensor as T
 from keras.preprocessing import text, sequence
-
+from six.moves import cPickle
 
 class WordEmbeddingModel(object):
     def __init__(self, words_limit=50000, dimension=128):
@@ -15,9 +16,11 @@ class WordEmbeddingModel(object):
         self.word_vectors = None
 
     def build_vocab(self, texts):
+        print "Building Vocabulary..."
         self.tokenizer.fit_on_texts(texts)
         self.words_limit = min(self.words_limit, len(self.tokenizer.word_counts))
         self.word_vectors = (random.randn(self.words_limit, self.dimension) - 0.5) / self.dimension
+        print "Finish building"
 
     def _sequentialize(self, texts, **kwargs):
         raise NotImplementedError()
@@ -38,6 +41,16 @@ class SkipGramNegSampEmbeddingModel(WordEmbeddingModel):
             yield sequence.skipgrams(seq, self.words_limit, window_size=self.window_size,
                                      negative_samples=self.neg_sample_rate,
                                      sampling_table=sampling_table)
+    def save_model(save_dir):
+        print "Save tokenizer..."
+        tokenizer_fname = "text8_tokenizer.pkl"
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        cPickle.dump(self.tokenizer, open(os.path.join(save_dir, tokenizer_fname), "wb"))
+
+        print "Save word vectors..."
+        WV_fname = "text8_WV.pkl"
+        cPickle.dump(self.word_vectors, open(os.path.join(save_dir, WV_fname), "wb"))
 
     def fit(self, texts, lrate=.1, sampling=True, **kwargs):
         x = T.dvector("x")
@@ -57,8 +70,21 @@ class SkipGramNegSampEmbeddingModel(WordEmbeddingModel):
             inputs=[x, y, u],
             outputs=gx,
             updates=((w, T.inc_subtensor(wu, lrate * gw)), (b, T.inc_subtensor(bu, lrate * gb))))
-
+        print "Start Training"
+        count  = 0
         for couples, labels in self._sequentialize(texts, sampling):
             for i, (wi, wj) in enumerate(couples):
                 delta_wv = train(self.word_vectors[wi - 1], labels[i], wj - 1)
                 self.word_vectors[wi - 1] += lrate * delta_wv
+                count += 1
+                if count%100 == 0
+                    print "Trained", count*100, "couples"
+        print "Finish Training"
+        save_dir = ""
+        print "Save weights"
+        weights_fname = "text8_weights.pkl"
+        bias_fname = "text8_bias.pkl"
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        cPickle.dump(w, open(os.path.join(save_dir, weights_fname), "wb"))
+        cPickle.dump(b, open(os.path.join(save_dir, bias_fname), "wb"))
