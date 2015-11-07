8f6eae9ff825bb90d8bed6e209313a92e4b1d3df
Jason Fan <sevengram1991@gmail.com>
2015-11-07 00:41:09 -0700

    update framwork
    1. remove keras import and model
    2. add sequence.py and text.py from keras
    3. change arguments
    4. change rules for output files
    https://github.com/fenfei/multi-sense-we/wiki/output

--- models.py
+++ models.py
@@ -7,12 +7,9 @@ import theano
 from numpy import random, zeros, linalg
 from theano import tensor as T
 
-from keras.models import Sequential
-from keras.optimizers import SGD
-from keras.utils import generic_utils
-from keras.preprocessing import text, sequence
+import sequence
 
-from layers import WordContextLayer
+import text
 
 
 class WordEmbeddingModel(object):
@@ -20,7 +17,6 @@ class WordEmbeddingModel(object):
         self.words_limit = words_limit
         self.dimension = dimension
         self.tokenizer = None
-        self.word_list = None
         self.wordvec_matrix = None
         self.weight_matrix = None
         self.biases = None
@@ -28,18 +24,16 @@ class WordEmbeddingModel(object):
     def init_values(self):
         self.wordvec_matrix = (random.randn(self.words_limit, self.dimension) - 0.5) / self.dimension
         self.weight_matrix = zeros((self.words_limit, self.dimension))
-        self.biases = zeros((self.words_limit, 1))
+        self.biases = zeros(self.words_limit)
 
-    def build_vocab(self, texts, spare_size=1):
-        self.tokenizer = text.Tokenizer(nb_words=self.words_limit)
+    def build_vocab(self, texts):
+        self.tokenizer = text.Tokenizer(words_limit=self.words_limit)
         self.tokenizer.fit_on_texts(texts)
-        self.words_limit = min(self.words_limit, len(self.tokenizer.word_counts)) + spare_size
-        self._build_word_list()
+        self.words_limit = min(self.words_limit, len(self.tokenizer.word_counts))
 
-    def load_vocab(self, path, spare_size=1):
+    def load_vocab(self, path):
         self.tokenizer = cPickle.load(open(path, 'rb'))
-        self.words_limit = min(self.words_limit, len(self.tokenizer.word_counts)) + spare_size
-        self._build_word_list()
+        self.words_limit = min(self.words_limit, len(self.tokenizer.word_counts))
 
     def load_word_vectors(self, path):
         self.wordvec_matrix = cPickle.load(open(path, 'rb'))
@@ -47,6 +41,14 @@ class WordEmbeddingModel(object):
     def save_tokenizer(self, path):
         if path:
             cPickle.dump(self.tokenizer, open(path, "wb"))
+
+    def save_word_list(self, path):
+        if path:
+            cPickle.dump(self.tokenizer.word_list, open(path, "wb"))
+
+    def save_word_index(self, path):
+        if path:
+            cPickle.dump(self.tokenizer.word_index, open(path, "wb"))
 
     def save_word_vectors(self, path):
         if path:
@@ -58,21 +60,18 @@ class WordEmbeddingModel(object):
     def _sequentialize(self, texts, **kwargs):
         raise NotImplementedError()
 
-    def _build_word_list(self):
-        self.word_list = {v: k for k, v in self.tokenizer.word_index.iteritems()}
-
     def nearest_words(self, word, limit=20):
         if self.tokenizer is None or self.wordvec_matrix is None:
             print('load vocab and model first!')
             return None
         word_index = self.tokenizer.word_index.get(word)
-        if word_index is None or word_index - 1 >= self.wordvec_matrix.shape[0]:
+        if word_index is None or word_index >= self.wordvec_matrix.shape[0]:
             print('can\'t find this word!')
             return None
         else:
-            d = [linalg.norm(self.wordvec_matrix[word_index - 1] - v) for v in self.wordvec_matrix]
+            d = [linalg.norm(self.wordvec_matrix[word_index] - v) for v in self.wordvec_matrix]
             nearest_indices = numpy.argpartition(d, limit)[:limit]
-            return {self.word_list[i + 1]: d[i] for i in nearest_indices}
+            return {self.tokenizer.word_list[i]: d[i] for i in nearest_indices}
 
 
 class SkipGramNegSampEmbeddingModel(WordEmbeddingModel):
@@ -107,34 +106,10 @@ class SkipGramNegSampEmbeddingModel(WordEmbeddingModel):
         for e in range(nb_epoch):
             for couples, labels in self._sequentialize(texts, sampling):
                 for i, (wi, wj) in enumerate(couples):
-                    dx, dw, db = train(self.wordvec_matrix[wi - 1],
-                                       labels[e],
-                                       self.weight_matrix[wj - 1],
-                                       self.biases[wj - 1])
-                    self.wordvec_matrix[wi - 1] += lrate * dx
-                    self.weight_matrix[wj - 1] += lrate * dw
-                    self.biases[wj - 1] += lrate * db
-
-
-class KerasEmbeddingModel(SkipGramNegSampEmbeddingModel):
-    def fit(self, texts, nb_epoch=1, lrate=.1, sampling=True, **kwargs):
-        self.init_values()
-        model = Sequential()
-        model.add(
-            WordContextLayer(self.words_limit, self.dimension, self.wordvec_matrix, self.weight_matrix, self.biases))
-        sgd = SGD(lr=lrate)
-        model.compile(loss='mse', optimizer=sgd)
-
-        progbar = generic_utils.Progbar(self.tokenizer.document_count)
-        losses = []
-        for e in range(nb_epoch):
-            for i, (couples, labels) in enumerate(self._sequentialize(texts, sampling)):
-                if couples:
-                    d = numpy.array(couples, dtype="int32")
-                    loss = model.train_on_batch(d, labels)
-                    losses.append(loss)
-                    if len(losses) % 100 == 0:
-                        progbar.update(i, values=[("loss", numpy.mean(losses))])
-                        losses = []
-
-        self.wordvec_matrix, self.weight_matrix, self.biases = model.layers[0].get_weights()
+                    dx, dw, db = train(self.wordvec_matrix[wi],
+                                       labels[i],
+                                       self.weight_matrix[wj],
+                                       self.biases[wj])
+                    self.wordvec_matrix[wi] += lrate * dx
+                    self.weight_matrix[wj] += lrate * dw
+                    self.biases[wj] += lrate * db
---
+++ text.py
@@ -0,0 +1,69 @@
+# -*- coding:utf-8 -*-
+
+import string
+
+
+def base_filter():
+    f = string.punctuation
+    f = f.replace("'", '')
+    f += '\t\n'
+    return f
+
+
+def text_to_word_sequence(text, filters=base_filter(), lower=True, split=" "):
+    """
+    prune: sequence of characters to filter out
+    """
+    if lower:
+        text = text.lower()
+    text = text.translate(string.maketrans(filters, split * len(filters)))
+    seq = text.split(split)
+    return [_f for _f in seq if _f]
+
+
+class Tokenizer(object):
+    def __init__(self, words_limit=None, filters=base_filter(), lower=True, split=" "):
+        self.word_counts = {}
+        self.word_index = {}
+        self.word_list = []
+        self.filters = filters
+        self.split = split
+        self.lower = lower
+        self.words_limit = words_limit
+
+    def fit_on_texts(self, texts):
+        """
+            required before using texts_to_sequences or texts_to_matrix
+            @param texts: can be a list or a generator (for memory-efficiency)
+        """
+        for text in texts:
+            seq = text_to_word_sequence(text, self.filters, self.lower, self.split)
+            for w in seq:
+                if w in self.word_counts:
+                    self.word_counts[w] += 1
+                else:
+                    self.word_counts[w] = 1
+        wcounts = list(self.word_counts.items())
+        wcounts.sort(key=lambda x: x[1], reverse=True)
+        self.word_list = [wc[0] for wc in wcounts]
+        self.word_index = dict(zip(self.word_list, range(len(self.word_list))))
+
+    def texts_to_sequences_generator(self, texts):
+        """
+            Transform each text in texts in a sequence of integers.
+            Only top "nb_words" most frequent words will be taken into account.
+            Only words known by the tokenizer will be taken into account.
+
+            Yields individual sequences.
+        """
+        for text in texts:
+            seq = text_to_word_sequence(text, self.filters, self.lower, self.split)
+            vect = []
+            for w in seq:
+                i = self.word_index.get(w)
+                if i is not None:
+                    if self.words_limit and i >= self.words_limit:
+                        pass
+                    else:
+                        vect.append(i)
+            yield vect
